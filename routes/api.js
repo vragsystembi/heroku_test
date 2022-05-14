37c7689fb52b0a7db041f4254cc7759761249dd0
Danik <chapagana@gmail.com>
2022-05-14 20:00:26 +0600

    added login and jwt session (in progress)

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,9 @@ const bcrypt = require("bcrypt");
 const uri =
   "mongodb+srv://aliba:aliba@projects.kfyhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
+const JWT_SECRET =
+  "@ALIBEK@SLAVE@fleksml13EC3k2mk@#mlkl@ALIBEK@SLAVE@fmeSM2f4mk2m4@#Krn2k#@ALIBEK@SLAVE@";
+
 router.route("/signup").post(async (req, res) => {
   const client = new MongoClient(uri);
   console.log(req.body);
@@ -40,6 +43,49 @@ router.route("/signup").post(async (req, res) => {
     });
     res.status(201).json({ token, username, email: sanitizedEmail });
   } catch (err) {
+    console.log(err);
+  }
+});
+
+router.route("/login").post(async (req, res) => {
+  const client = new MongoClient(uri);
+  const { email, password } = req.body;
+
+  try {
+    await client.connect();
+    const database = client.db("skillcheck");
+    const users = database.collection("users");
+
+    const user = await users.findOne({ email });
+
+    if (!user) {
+      console.log("no such user");
+      return res.json({
+        status: "error",
+        error: "Invalid username/password or user does not exist1",
+      });
+    }
+
+    if (await bcrypt.compare(password, user.hashed_password)) {
+      // the username, password combination is successful
+
+      console.log(1);
+
+      const token = jwt.sign(
+        {
+          id: user._id,
+          email: user.email,
+        },
+        JWT_SECRET
+      );
+
+      return res.json({ status: "ok", data: token });
+    }
+  } catch (err) {
+    res.json({
+      status: "error",
+      error: "Invalid username/password or user does not exist2",
+    });
     console.log(err);
   }
 });
